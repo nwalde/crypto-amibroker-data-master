e79fd5b5a5d91c7d16befd14a9a8aa35f58e5a19
cyrilpanicker <cyrilpanicker@gmail.com>
2017-12-23 23:46:21 +0530

    first-commit

---
+++ main.js
@@ -0,0 +1,44 @@
+const axios = require('axios');
+const fs = require('fs');
+const json2csv = require('json2csv');
+
+axios('https://bittrex.com/api/v1.1/public/getmarkets').then(
+    response => {
+        response.data.result.forEach((market,index) => {
+            if(market.MarketName.indexOf('BTC-')>-1){
+                const url = 'https://bittrex.com/api/v2.0/pub/market/getticks?marketname='+market.MarketName+'&tickInterval=day';
+                (ticker=>{
+                    axios(url).then(
+                        response => {
+                            if(!response.data.success){
+                                console.log('FETCH FAILED FOR : '+market.MarketName);
+                            }else{
+                                const csvData = json2csv({
+                                    data:response.data.result,
+                                    fields:[
+                                        {value:row=>ticker},
+                                        {value:row=>row['T'].replace('T00:00:00','')},
+                                        {value:row=>row['O'].toFixed(8),stringify:false},
+                                        {value:row=>row['H'].toFixed(8),stringify:false},
+                                        {value:row=>row['L'].toFixed(8),stringify:false},
+                                        {value:row=>row['C'].toFixed(8),stringify:false},
+                                        {value:row=>row['V']}
+                                    ],
+                                    hasCSVColumnTitle:false,
+                                    quotes:''
+                                });
+                                fs.writeFile('results/'+market.MarketName+'.txt',csvData,'utf8',(error)=>{
+                                    if(error){
+                                        console.log('WRITE FAILED FOR : '+market.MarketName);
+                                    }
+                                });
+                            }
+                        },
+                        error => console.log(error)
+                    );
+                })(market.MarketName)
+            }
+        });
+    },
+    error => console.log(error)
+);
