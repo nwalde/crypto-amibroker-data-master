fadd04e95c0040dd94b24675af2176750da47d78
Cyril Sadasivan <cyril.sadasivan@litmus7.com>
2017-12-24 00:58:42 +0530

    added changes

--- main.js
+++ main.js
@@ -7,36 +7,34 @@ axios('https://bittrex.com/api/v1.1/public/getmarkets').then(
         response.data.result.forEach((market,index) => {
             if(market.MarketName.indexOf('BTC-')>-1){
                 const url = 'https://bittrex.com/api/v2.0/pub/market/getticks?marketname='+market.MarketName+'&tickInterval=day';
-                (ticker=>{
-                    axios(url).then(
-                        response => {
-                            if(!response.data.success){
-                                console.log('FETCH FAILED FOR : '+market.MarketName);
-                            }else{
-                                const csvData = json2csv({
-                                    data:response.data.result,
-                                    fields:[
-                                        {value:row=>ticker},
-                                        {value:row=>row['T'].replace('T00:00:00','')},
-                                        {value:row=>row['O'].toFixed(8),stringify:false},
-                                        {value:row=>row['H'].toFixed(8),stringify:false},
-                                        {value:row=>row['L'].toFixed(8),stringify:false},
-                                        {value:row=>row['C'].toFixed(8),stringify:false},
-                                        {value:row=>row['V']}
-                                    ],
-                                    hasCSVColumnTitle:false,
-                                    quotes:''
-                                });
-                                fs.writeFile('results/'+market.MarketName+'.txt',csvData,'utf8',(error)=>{
-                                    if(error){
-                                        console.log('WRITE FAILED FOR : '+market.MarketName);
-                                    }
-                                });
-                            }
-                        },
-                        error => console.log(error)
-                    );
-                })(market.MarketName)
+                axios(url).then(
+                    response => {
+                        if(!response.data.success){
+                            console.log('FETCH FAILED FOR : '+market.MarketName);
+                        }else{
+                            const csvData = json2csv({
+                                data:response.data.result,
+                                fields:[
+                                    {value:row=>market.MarketName},
+                                    {value:row=>row['T'].replace('T00:00:00','')},
+                                    {value:row=>row['O'].toFixed(8),stringify:false},
+                                    {value:row=>row['H'].toFixed(8),stringify:false},
+                                    {value:row=>row['L'].toFixed(8),stringify:false},
+                                    {value:row=>row['C'].toFixed(8),stringify:false},
+                                    {value:row=>row['V']}
+                                ],
+                                hasCSVColumnTitle:false,
+                                quotes:''
+                            });
+                            fs.writeFile('results/'+market.MarketName+'.txt',csvData,'utf8',(error)=>{
+                                if(error){
+                                    console.log('WRITE FAILED FOR : '+market.MarketName);
+                                }
+                            });
+                        }
+                    },
+                    error => console.log('FETCH FAILED FOR : '+market.MarketName)
+                );
             }
         });
     },
