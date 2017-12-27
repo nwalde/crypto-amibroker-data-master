073636aa66487df6216829c73c6539681db981f5
nwalde <nwalde@monitechnologies.com>
2017-12-27 22:23:12 +0530

    changed script to download only BTC-USD data

--- main.js
+++ main.js
@@ -2,41 +2,31 @@ const axios = require('axios');
 const fs = require('fs');
 const json2csv = require('json2csv');
 
-axios('https://bittrex.com/api/v1.1/public/getmarkets').then(
+const url = 'https://bittrex.com/api/v2.0/pub/market/getticks?marketname=usdt-btc&tickinterval=hour';
+axios(url).then(
     response => {
-        response.data.result.forEach((market,index) => {
-            if(market.MarketName.indexOf('BTC-')>-1){
-                const url = 'https://bittrex.com/api/v2.0/pub/market/getticks?marketname='+market.MarketName+'&tickInterval=day';
-                axios(url).then(
-                    response => {
-                        if(!response.data.success){
-                            console.log('FETCH FAILED FOR : '+market.MarketName);
-                        }else{
-                            const csvData = json2csv({
-                                data:response.data.result,
-                                fields:[
-                                    {value:row=>market.MarketName},
-                                    {value:row=>row['T'].replace('T00:00:00','')},
-                                    {value:row=>row['O'].toFixed(8),stringify:false},
-                                    {value:row=>row['H'].toFixed(8),stringify:false},
-                                    {value:row=>row['L'].toFixed(8),stringify:false},
-                                    {value:row=>row['C'].toFixed(8),stringify:false},
-                                    {value:row=>row['V']}
-                                ],
-                                hasCSVColumnTitle:false,
-                                quotes:''
-                            });
-                            fs.writeFile('results/'+market.MarketName+'.txt',csvData,'utf8',(error)=>{
-                                if(error){
-                                    console.log('WRITE FAILED FOR : '+market.MarketName);
-                                }
-                            });
-                        }
-                    },
-                    error => console.log('FETCH FAILED FOR : '+market.MarketName)
-                );
-            }
-        });
+        if(!response.data.success){
+            console.log('FETCH FAILED FOR : BTC-USDT');
+        }else{
+            const csvData = json2csv({
+                data:response.data.result,
+                fields:[
+                    {value:row=>row['T'].replace('T',',')},
+                    {value:row=>row['O'].toFixed(8),stringify:false},
+                    {value:row=>row['H'].toFixed(8),stringify:false},
+                    {value:row=>row['L'].toFixed(8),stringify:false},
+                    {value:row=>row['C'].toFixed(8),stringify:false},
+                    {value:row=>row['V']}
+                ],
+                hasCSVColumnTitle:false,
+                quotes:''
+            });
+            fs.writeFile('BTC-USDT.csv',csvData,'utf8',(error)=>{
+                if(error){
+                    console.log('WRITE FAILED FOR : BTC-USDT');
+                }
+            });
+        }
     },
-    error => console.log(error)
+    error => console.log('FETCH FAILED FOR : BTC-USDT')
 );
